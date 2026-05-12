aac93f0a1813001c1f9e4fdddb85e1d9829231f0
Adebayo Peter Oluwagbemileke <adebayopetergvmc68@gmail.com>
2026-05-12 07:52:10 +0100

    feat: replace WhatsApp RSVP integration with prefilled Google Forms submission

--- frontend/js/wdp-rsvp-form4963.js
+++ frontend/js/wdp-rsvp-form4963.js
@@ -11,41 +11,62 @@ jQuery(document).ready(function($){
     /* Home RSVP Form */
     $('#wdp_home_form').on('submit', function(e) {
         e.preventDefault();
+        e.stopImmediatePropagation();
 
+        var form = $(this);
         var name = $('#name').val();
         var guests = $('#guests').val();
-        var events = $('#events option:selected').text();
-        
-        var message = "Hi, My name is " + name + ".\nI will be Attending your wedding.\nNo of guest: " + guests + ".\nAttending: " + events;
-        var encodedMessage = encodeURIComponent(message);
-        var phoneNumber = "2348102042019";
-        
-        var whatsappUrl = "https://wa.me/" + phoneNumber + "?text=" + encodedMessage;
-        
-        window.open(whatsappUrl, '_blank');
+        var events = $('#events').val();
 
-        $('.wdp_home_form_success').addClass('show animated fadeInUp');
-        $(this).hide();
+        // Map 'Reception' to 'Redception' to match the typo on the Google Form radio button precisely
+        if (events === "Reception") {
+            events = "Redception";
+        }
+
+        var baseUrl = "https://docs.google.com/forms/d/e/1FAIpQLSectwsX6ROF3ikKA2cqouAtbP-aaO7K__Ot4OCXGmFHnHE2Fg/viewform";
+        var params = new URLSearchParams();
+        params.append("usp", "pp_url");
+        params.append("entry.1498135098", name);
+        params.append("entry.2606285", guests);
+        params.append("entry.1214077163", events);
+
+        var prefillUrl = baseUrl + "?" + params.toString();
+
+        // Open the prefilled Google Form in a new tab for final verified submission
+        window.open(prefillUrl, '_blank');
+
+        // Reset the local form inputs
+        form[0].reset();
+        form.find('select').trigger('update.fs');
     });
 
     /* Page RSVP Form */
     $('#wdp_page_form').on('submit', function(e) {
         e.preventDefault();
-        
-        var name = $(this).find('input[name="name"]').val();
-        var guests = $(this).find('input[name="guests"]').val();
-        var events = $(this).find('select[name="events"] option:selected').text();
-        
-        var message = "Hi, My name is " + name + ".\nI will be Attending your wedding.\nNo of guest: " + guests + ".\nAttending: " + events;
-        var encodedMessage = encodeURIComponent(message);
-        var phoneNumber = "2348102042019";
-        
-        var whatsappUrl = "https://wa.me/" + phoneNumber + "?text=" + encodedMessage;
-        
-        window.open(whatsappUrl, '_blank');
+        e.stopImmediatePropagation();
 
-        $('.wdp_page_form_success').addClass('show animated fadeInUp');
-        $(this).hide();
+        var form = $(this);
+        var name = form.find('input[name*="entry."], input[name="name"]').val();
+        var guests = form.find('input[name*="entry."], input[name="guests"]').val();
+        var events = form.find('select').val();
+
+        if (events === "Reception") {
+            events = "Redception";
+        }
+
+        var baseUrl = "https://docs.google.com/forms/d/e/1FAIpQLSectwsX6ROF3ikKA2cqouAtbP-aaO7K__Ot4OCXGmFHnHE2Fg/viewform";
+        var params = new URLSearchParams();
+        params.append("usp", "pp_url");
+        params.append("entry.1498135098", name);
+        params.append("entry.2606285", guests);
+        params.append("entry.1214077163", events);
+
+        var prefillUrl = baseUrl + "?" + params.toString();
+
+        window.open(prefillUrl, '_blank');
+
+        form[0].reset();
+        form.find('select').trigger('update.fs');
     });
 
 	/* FancySelect */
